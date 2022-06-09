d7f65019460ad45e3f98e73b0d2832e8055f2887
Sammy <sammysamkough@hotmail.com>
2022-06-09 00:03:05 -0700

    switched to next

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import type { AppProps } from 'next/app';
+import React from 'react';
+import '../styles/globals.css';
+
+const _app = ({ Component, pageProps }: AppProps): React.ReactElement => <Component {...pageProps} />
+
+export default _app;
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = (): React.ReactElement => (
+  <div><p>test</p></div>
+)
+
+export default Home
